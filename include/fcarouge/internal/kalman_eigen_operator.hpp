39cd7bd5b6522ac0f97cf485e55629977f2c50ab
François Carouge <francois.carouge@gmail.com>
2022-06-15 18:27:43 -0700

    [CPO] identify operation as default customization point objects

--- include/fcarouge/internal/kalman_eigen_operator.hpp
+++ include/fcarouge/internal/kalman_eigen_operator.hpp
@@ -41,6 +41,8 @@ For more information, please refer to <https://unlicense.org> */
 
 //! @file
 //! @brief Kalman operation for Eigen 3 types.
+//!
+//! @details Default customization point objects (CPO) for Eigen 3 types.
 
 #include "kalman.hpp"
 
