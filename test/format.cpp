2b2f35cdcc6a7628e837254acbf0ee93fbb466e3
Francois Carouge <francois.carouge@gmail.com>
2022-07-10 19:08:46 -0700

    [format] add standard format support

---
+++ test/format.cpp
@@ -0,0 +1,64 @@
+/*_  __          _      __  __          _   _
+ | |/ /    /\   | |    |  \/  |   /\   | \ | |
+ | ' /    /  \  | |    | \  / |  /  \  |  \| |
+ |  <    / /\ \ | |    | |\/| | / /\ \ | . ` |
+ | . \  / ____ \| |____| |  | |/ ____ \| |\  |
+ |_|\_\/_/    \_\______|_|  |_/_/    \_\_| \_|
+
+Kalman Filter for C++
+Version 0.1.0
+https://github.com/FrancoisCarouge/Kalman
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/kalman.hpp"
+#include "fcarouge/kalman_eigen.hpp"
+
+#include <cassert>
+#include <format>
+#include <string>
+
+namespace fcarouge::test
+{
+namespace
+{
+
+//! @test Verifies formatting filters for single-dimension filters with input
+//! control.
+[[maybe_unused]] auto format111{ [] {
+  using kalman = fcarouge::kalman<double, double, double>;
+  kalman k;
+
+  assert(std::format("{}", k) ==
+         "{f:1,g:1,h:1,k:1,p:1,q:0,r:0,s:1,u:0,x:0,y:0,z:0}");
+
+  return 0;
+}() };
+
+} // namespace
+} // namespace fcarouge::test
