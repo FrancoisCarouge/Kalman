1420ff13d267c76634afae94949f26789f5277d8
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2022-08-09 02:24:36 +0000

    deploy: 80ed4a732e2f06c0a785c778e45af5a1a153a9ad

--- navtreedata.js
+++ navtreedata.js
@@ -44,28 +44,33 @@ var NAVTREE =
       [ "Installation", "index.xhtml#autotoc_md27", null ],
       [ "Motivation", "index.xhtml#autotoc_md28", null ],
       [ "Resources", "index.xhtml#autotoc_md29", null ],
-      [ "License", "index.xhtml#autotoc_md30", null ]
+      [ "Third Party Acknowledgement", "index.xhtml#autotoc_md30", null ],
+      [ "Sponsors", "index.xhtml#autotoc_md31", [
+        [ "Corporate Sponsor", "index.xhtml#autotoc_md32", null ],
+        [ "Named Sponsors", "index.xhtml#autotoc_md33", null ]
+      ] ],
+      [ "License", "index.xhtml#autotoc_md34", null ]
     ] ],
     [ "Contributor Covenant Code of Conduct", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml", [
-      [ "Our Pledge", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md6", null ],
-      [ "Our Standards", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md7", null ],
-      [ "Our Responsibilities", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md8", null ],
+      [ "Our Pledge", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md1", null ],
+      [ "Our Standards", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md3", null ],
+      [ "Our Responsibilities", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md7", null ],
       [ "Scope", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md9", null ],
       [ "Enforcement", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md10", null ],
       [ "Attribution", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md11", null ]
     ] ],
     [ "Contributing", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml", [
-      [ "Code of Conduct", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md2", null ],
-      [ "Reporting Bugs", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md3", null ],
-      [ "Requesting Features", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md4", null ],
-      [ "Security Policy", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md5", null ]
+      [ "Code of Conduct", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md4", null ],
+      [ "Reporting Bugs", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md5", null ],
+      [ "Requesting Features", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md6", null ],
+      [ "Security Policy", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md8", null ]
     ] ],
     [ "Installation", "md__i_n_s_t_a_l_l.xhtml", [
       [ "System", "md__i_n_s_t_a_l_l.xhtml#autotoc_md13", null ]
     ] ],
     [ "Security Policy", "md__s_e_c_u_r_i_t_y.xhtml", [
-      [ "Supported Versions", "md__s_e_c_u_r_i_t_y.xhtml#autotoc_md32", null ],
-      [ "Reporting a Vulnerability", "md__s_e_c_u_r_i_t_y.xhtml#autotoc_md33", null ]
+      [ "Supported Versions", "md__s_e_c_u_r_i_t_y.xhtml#autotoc_md36", null ],
+      [ "Reporting a Vulnerability", "md__s_e_c_u_r_i_t_y.xhtml#autotoc_md37", null ]
     ] ],
     [ "Todo List", "todo.xhtml", null ],
     [ "Test List", "test.xhtml", null ],
@@ -103,7 +108,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.xhtml",
-"kalman__eigen_8hpp.xhtml#a19baaa3fe4523729db807408c3771dc3"
+"kalman_8hpp.xhtml#aa5a28afb4550bbcbd55d875351de6e27"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
