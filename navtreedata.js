3bbc636ed409bce468f24a4d4ac4906daf8dab63
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2022-10-16 01:27:18 +0000

    deploy: 68c6c2a038f8cfd0264197172ab275c58272d0b4

--- navtreedata.js
+++ navtreedata.js
@@ -61,7 +61,7 @@ var NAVTREE =
       [ "Results", "md_benchmark__r_e_a_d_m_e.xhtml#autotoc_md1", null ]
     ] ],
     [ "Contributor Covenant Code of Conduct", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml", [
-      [ "Our Pledge", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md8", null ],
+      [ "Our Pledge", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md7", null ],
       [ "Our Standards", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md9", null ],
       [ "Our Responsibilities", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md10", null ],
       [ "Scope", "md__c_o_d_e__o_f__c_o_n_d_u_c_t.xhtml#autotoc_md11", null ],
@@ -72,7 +72,7 @@ var NAVTREE =
       [ "Code of Conduct", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md4", null ],
       [ "Reporting Bugs", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md5", null ],
       [ "Requesting Features", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md6", null ],
-      [ "Security Policy", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md7", null ]
+      [ "Security Policy", "md__c_o_n_t_r_i_b_u_t_i_n_g.xhtml#autotoc_md8", null ]
     ] ],
     [ "Installation", "md__i_n_s_t_a_l_l.xhtml", [
       [ "System Library", "md__i_n_s_t_a_l_l.xhtml#autotoc_md15", null ],
