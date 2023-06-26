09fe3f0c8c52d34124badbc27241d3802ca875c6
FrancoisCarouge <francois.carouge@gmail.com>
2023-06-25 20:10:32 -0700

    [linalg] start adding linalg alternatives

---
+++ linalg/lazy/fcarouge/linalg.hpp
@@ -0,0 +1,380 @@
+/*  __          _      __  __          _   _
+| |/ /    /\   | |    |  \/  |   /\   | \ | |
+| ' /    /  \  | |    | \  / |  /  \  |  \| |
+|  <    / /\ \ | |    | |\/| | / /\ \ | . ` |
+| . \  / ____ \| |____| |  | |/ ____ \| |\  |
+|_|\_\/_/    \_\______|_|  |_/_/    \_\_| \_|
+
+Kalman Filter
+Version 0.2.0
+https://github.com/FrancoisCarouge/Kalman
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#ifndef FCAROUGE_LINALG_LAZY_HPP
+#define FCAROUGE_LINALG_LAZY_HPP
+
+//! @file
+//! @brief Linear algebra facade for a coroutine-based lazy implementation.
+//!
+//! @details Standardizes matrix, vectors, and algebraic values type names for
+//! the library usage.
+//!
+//! @note Idea from:
+//! https://gieseanw.wordpress.com/2019/10/20/we-dont-need-no-stinking-expression-templates/
+
+#include "fcarouge/utility.hpp"
+
+#include <algorithm>
+#include <array>
+#include <concepts>
+#include <coroutine>
+#include <generator>
+#include <ranges>
+#include <type_traits>
+#include <utility>
+
+namespace fcarouge {
+
+// Semantic? Guarantees? to_generator? std::ranges::to overload? make_generator?
+// Overloads for C-array?
+// Need one copy, avoid any extra?
+auto make_generator(auto elements)
+    -> std::generator<typename decltype(elements)::value_type>
+  requires(std::ranges::range<decltype(elements)>)
+{
+  return [](auto elements_copy)
+             -> std::generator<typename decltype(elements)::value_type> {
+    co_yield std::ranges::elements_of(elements_copy);
+  }(elements);
+}
+
+// Need one copy, avoid any extra?
+template <typename Type>
+auto make_generator(Type element) -> std::generator<Type> {
+  return [](Type element_copy) -> std::generator<Type> {
+    co_yield element_copy;
+  }(element);
+}
+
+//! @name Algebraic Types
+//! @{
+//! @brief Coroutine lazy matrix.
+//!
+//! @details The matrix is a generator. A coroutine range. Lazily generated
+//! elements and computed operations. Commonalities with ranges.
+//!
+//! @tparam Type The linear algebra underlying element type.
+//! @tparam Row The number of rows.
+//! @tparam Column The number of columns.
+//! @tparam Copyable Whether the instance is fully lazy or deeply clones itself
+//! on copies. Useful for named values. May not be commonly used.
+//!
+//! @note Lifetime management with coroutine is tricky. The generators cannot
+//! use the lambda captures. The parameters of the lambda can however be copied
+//! to guarentee their lifetime for the usage of the generator. This technique
+//! is useful for initialization of the generator.
+//! @note A design decision for the composed generator to be mutable, traded
+//! off for const member function API. Similar to the mutable mutex member
+//! practice.
+//! @note Why genie? Because genies generate on demand.
+//!
+//! @todo Explore and compare performance.
+//! @todo Explore optimization of heap allocations?
+//! @todo Explore constexpr support?
+//! @todo Explore verification of lazy evaluation?
+template <typename Type = double, auto Row = 1, auto Column = 1,
+          bool Copyable = false>
+struct matrix {
+  //! @todo Report the ICE for generator construction in member declaration. Use
+  //! constructor's initializer list for now.
+  inline constexpr matrix()
+      : genie{[] -> std::generator<Type> {
+          for (auto k{Row * Column}; k > 0; --k) { // repeat(Type{}) | take(R*C)
+            co_yield {};
+          }
+        }()} {}
+
+  inline constexpr matrix(const matrix &other)
+    requires(!Copyable)
+      : genie{std::move(other.genie)} {}
+
+  inline constexpr matrix(const matrix &other)
+    requires(Copyable)
+      : genie{other.clone()} {}
+
+  inline constexpr matrix(matrix &&other) = default;
+
+  inline constexpr matrix &operator=(const matrix &other)
+    requires(Copyable)
+  {
+    genie = other.clone();
+  }
+
+  inline constexpr matrix &operator=(const matrix &other)
+    requires(!Copyable)
+  {
+    genie = std::move(other.genie);
+    return *this;
+  }
+
+  inline constexpr matrix &operator=(matrix &&other)
+    requires(Copyable)
+  = delete;
+
+  inline constexpr explicit matrix(const std::same_as<Type> auto &...elements)
+    requires(sizeof...(elements) == Row * Column)
+      : genie{[](auto... elements_copy) -> std::generator<Type> {
+          (co_yield elements_copy, ...);
+        }(elements...)} {}
+
+  inline constexpr matrix(Type (&elements)[Row * Column])
+    requires(Row == 1 || Column == 1)
+      : genie{make_generator(std::to_array(elements))} {}
+
+  template <auto... Columns>
+  constexpr matrix(const Type (&...rows)[Columns])
+    requires((sizeof...(rows) == Row) && (((Columns == Column) && ... && true)))
+      : genie{[](auto rows_copy) -> std::generator<Type> {
+          for (auto &&row : rows_copy) {
+            co_yield std::ranges::elements_of(row);
+          }
+        }(std::to_array({std::to_array(rows)...}))} {}
+
+  // More of these? Swap?
+  inline constexpr matrix(std::generator<Type> &&other)
+      : genie{std::move(other)} {}
+
+  [[nodiscard]] inline constexpr std::generator<Type> clone() const {
+    std::array<Type, Row * Column> elements; // std::ranges::to
+    std::ranges::copy(genie, elements.begin());
+
+    genie = make_generator(elements);
+
+    return make_generator(elements);
+  }
+
+  [[nodiscard]] inline constexpr explicit(false)
+  operator matrix<Type, Row, Column, !Copyable>() const {
+    return clone();
+  }
+
+  [[nodiscard]] inline constexpr explicit(false) operator Type() const
+    requires(Row == 1 && Column == 1)
+  {
+    Type result{*genie.begin()};
+
+    genie = make_generator(result);
+
+    return result;
+  }
+
+  [[nodiscard]] inline constexpr explicit(false)
+  operator std::generator<Type>() const {
+    co_yield std::ranges::elements_of(genie);
+  }
+
+  [[nodiscard]] inline constexpr Type operator[](auto index) const
+    requires(Row > 1 && Column == 1)
+  {
+    std::array<Type, Row * Column> elements; // std::ranges::to
+
+    std::ranges::copy(genie, elements.begin());
+    genie = make_generator(elements);
+
+    return elements[index];
+  }
+
+  [[nodiscard]] inline constexpr Type operator[](auto index) const
+    requires(Row == 1)
+  {
+    std::array<Type, Row * Column> elements; // std::ranges::to
+
+    std::ranges::copy(genie, elements.begin());
+    genie = make_generator(elements);
+
+    return elements[index];
+  }
+
+  [[nodiscard]] inline constexpr Type operator()(auto index) const
+    requires(Row > 1 && Column == 1)
+  {
+    std::array<Type, Row * Column> elements; // std::ranges::to
+
+    std::ranges::copy(genie, elements.begin());
+    genie = make_generator(elements);
+
+    return elements[index];
+  }
+
+  [[nodiscard]] inline constexpr Type operator()(auto index) const
+    requires(Row == 1)
+  {
+    std::array<Type, Row * Column> elements; // std::ranges::to
+    std::ranges::copy(genie, elements.begin());
+
+    genie = make_generator(elements);
+
+    return elements[index];
+  }
+
+  //! @todo Don't evaluate unless needed. How to do? Some kind of recursive
+  //! genie recomposer?
+  [[nodiscard]] inline constexpr Type operator()(auto row, auto column) const {
+    std::array<Type, Row * Column> elements; // std::ranges::to
+
+    std::ranges::copy(genie, elements.begin());
+    genie = make_generator(elements);
+
+    return elements[row * Column + column];
+  }
+
+  using generator = std::generator<Type>;
+  using promise_type = std::coroutine_traits<generator>::promise_type;
+  // Add other aliases such as iterator or value type?
+
+  auto begin() const { return genie.begin(); }
+  auto end() const { return genie.end(); }
+
+  //! @todo Remove optional wrapper for the `empty` API support of the
+  //! `std::generator` support and simplify the implementation.
+  //! @todo Clarify requirements for the mutable nature of the generator?
+  //! Remove.
+  mutable generator genie;
+};
+
+//! @brief Coroutine lazy row vector.
+template <typename Type = double, auto Column = 1, bool Copyable = false>
+using row_vector = matrix<Type, decltype(Column){1}, Column, Copyable>;
+
+//! @brief Coroutine lazy column vector.
+template <typename Type = double, auto Row = 1, bool Copyable = false>
+using column_vector = matrix<Type, Row, decltype(Row){1}, Copyable>;
+
+//! @}
+
+//! @name Deduction Guides
+//! @{
+template <typename Type> matrix(Type) -> matrix<Type, 1, 1>;
+
+template <typename Type, auto Row, auto Column>
+matrix(const Type (&)[Row][Column]) -> matrix<Type, Row, Column>;
+
+template <typename Type, auto Row>
+matrix(const Type (&)[Row]) -> matrix<Type, Row, 1>;
+
+// Fix me. Test me.
+template <typename... Types, auto... Columns>
+matrix(const Types (&...rows)[Columns])
+    -> matrix<std::common_type_t<Types...>, sizeof...(Columns),
+              (Columns * ... * 1) / sizeof...(Columns)>;
+//! @}
+
+//! @name Algebraic Named Values
+//! @{
+//! @brief The identity matrix.
+template <typename Type, auto Row, auto Column>
+matrix<Type, Row, Column, true> identity_v<matrix<Type, Row, Column>>{
+    [] -> std::generator<Type> {
+      for (decltype(Row) i{0}; i < Row; ++i) {
+        for (decltype(Column) j{0}; j < Column; ++j) {
+          co_yield i == j;
+        }
+      }
+    }()};
+
+//! @brief The zero matrix.
+template <typename Type, auto Row, auto Column>
+matrix<Type, Row, Column, true> zero_v<matrix<Type, Row, Column>>{
+    [] -> std::generator<Type> {
+      for (auto k{Row * Column}; k > 0; --k) {
+        co_yield 0.0;
+      }
+    }()};
+//! @}
+
+template <typename Type, auto Row, auto Column, bool CopyableOrNot1,
+          bool CopyableOrNot2>
+[[nodiscard]] inline constexpr bool
+operator==(matrix<Type, Row, Column, CopyableOrNot1> lhs,
+           matrix<Type, Row, Column, CopyableOrNot2> rhs) {
+  std::array<Type, Row * Column> lhs_elements; // std::ranges::to
+  std::ranges::copy(lhs.genie, lhs_elements.begin());
+  lhs.genie = make_generator(lhs_elements);
+
+  std::array<Type, Row * Column> rhs_elements; // std::ranges::to
+  std::ranges::copy(rhs.genie, rhs_elements.begin());
+  rhs.genie = make_generator(rhs_elements);
+
+  return lhs_elements == rhs_elements;
+}
+
+template <typename Type, auto Row, auto Size>
+[[nodiscard]] inline constexpr matrix<Type, Row, 1>
+operator*(matrix<Type, Row, Size> lhs, matrix<Type, Size, 1> rhs) {
+  // fix me?
+  auto next1{lhs.begin()};
+  for (decltype(Row) i{0}; i < Row; ++i) {       // chunk_by_rows
+    matrix<Type, Size, 1> rhs_copy{rhs.clone()}; // repeat_n
+    auto next2{rhs_copy.begin()};
+    Type result{0}; // inner_product?
+    for (decltype(Size) k{0}; k < Size; ++k, ++next1, ++next2) {
+      result += *next1 * *next2;
+    }
+    co_yield result;
+  }
+}
+
+template <typename Type, auto Row, auto Column>
+[[nodiscard]] inline constexpr matrix<Type, Row, Column>
+operator*(matrix<Type, Row, Column> lhs, arithmetic auto rhs) {
+  auto next{lhs.begin()};
+  for (auto k{Row * Column}; k > 0; --k, ++next) {
+    co_yield *next *rhs;
+  }
+}
+
+template <typename Type>
+[[nodiscard]] inline constexpr matrix<Type, 1, 1>
+operator+(Type lhs, matrix<Type, 1, 1> rhs) {
+  co_yield lhs + *rhs.begin();
+}
+
+template <typename Type, auto Row, auto Column>
+[[nodiscard]] inline constexpr matrix<Type, Row, Column>
+operator+(matrix<Type, Row, Column> lhs, matrix<Type, Row, Column> rhs) {
+  auto next1{lhs.begin()};
+  auto next2{rhs.begin()};
+  for (auto k{Row * Column}; k > 0; --k, ++next1, ++next2) {
+    co_yield *next1 + *next2;
+  }
+}
+
+} // namespace fcarouge
+
+#endif // FCAROUGE_LINALG_LAZY_HPP
---
+++ linalg/naive/fcarouge/linalg.hpp
@@ -0,0 +1,327 @@
+/*  __          _      __  __          _   _
+| |/ /    /\   | |    |  \/  |   /\   | \ | |
+| ' /    /  \  | |    | \  / |  /  \  |  \| |
+|  <    / /\ \ | |    | |\/| | / /\ \ | . ` |
+| . \  / ____ \| |____| |  | |/ ____ \| |\  |
+|_|\_\/_/    \_\______|_|  |_/_/    \_\_| \_|
+
+Kalman Filter
+Version 0.2.0
+https://github.com/FrancoisCarouge/Kalman
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#ifndef FCAROUGE_LINALG_HPP
+#define FCAROUGE_LINALG_HPP
+
+#include "fcarouge/utility.hpp"
+
+#include <initializer_list>
+
+namespace fcarouge {
+//! @name Algebraic Types
+//! @{
+//! @brief Matrix.
+template <typename Type = double, auto Row = 1, auto Column = 1> struct matrix {
+  inline constexpr matrix() = default;
+
+  inline constexpr explicit(false) matrix(Type element)
+    requires(Row == 1 && Column == 1)
+  {
+    data[0][0] = element;
+  }
+
+  inline constexpr explicit matrix(Type(element)[1])
+    requires(Row == 1 && Column == 1)
+  {
+    data[0][0] = element[0];
+  }
+
+  inline constexpr explicit matrix(Type(column)[Row])
+    requires(Row > 1 && Column == 1)
+  {
+    for (decltype(Row) i{0}; i < Row; ++i) {
+      data[i][0] = column[i];
+    }
+  }
+
+  inline constexpr explicit matrix(Type(row)[Column])
+    requires(Row == 1 && Column > 1)
+  {
+    for (decltype(Column) j{0}; j < Column; ++j) {
+      data[0][j] = row[j];
+    }
+  }
+
+  inline constexpr matrix(const auto &...elements)
+    requires(Row > 1 && Column == 1 && sizeof...(elements) == Row)
+  {
+    decltype(Row) i{0};
+    ([&] { data[i++][0] = elements; }(), ...);
+  }
+
+  inline constexpr matrix(const auto &...elements)
+    requires(Row == 1 && Column > 1 && sizeof...(elements) == Column)
+  {
+    decltype(Column) j{0};
+    ([&] { data[0][j++] = elements; }(), ...);
+  }
+
+  inline constexpr explicit matrix(
+      const std::initializer_list<std::initializer_list<Type>> &rows) {
+    // static_assert
+    for (decltype(Row) i{0}; auto &&elements : rows) {
+      for (decltype(Column) j{0}; auto &&element : elements) {
+        data[i][j++] = element;
+      }
+      ++i;
+    }
+  }
+
+  inline constexpr explicit(false) operator Type() const
+    requires(Row == 1 && Column == 1)
+  {
+    return data[0][0];
+  }
+
+  inline constexpr const Type &operator[](auto index) const
+    requires(Row > 1 && Column == 1)
+  {
+    return data[index][0];
+  }
+
+  inline constexpr const Type &operator[](auto index) const
+    requires(Row == 1)
+  {
+    return data[0][index];
+  }
+
+  inline constexpr const Type &operator()(auto index) const
+    requires(Row > 1 && Column == 1)
+  {
+    return data[index][0];
+  }
+
+  inline constexpr const Type &operator()(auto index) const
+    requires(Row == 1)
+  {
+    return data[0][index];
+  }
+
+  inline constexpr const Type &operator()(auto row, auto column) const {
+    return data[row][column];
+  }
+
+  [[nodiscard]] inline constexpr bool operator==(const matrix &other) const
+    requires(Row > 1 || Column > 1)
+  = default;
+
+  [[no_unique_address]] Type data[Row][Column]{};
+};
+
+//! @brief Row vector.
+template <typename Type = double, auto Column = 1>
+using row_vector = matrix<Type, decltype(Column){1}, Column>;
+
+//! @brief Column vector.
+template <typename Type = double, auto Row = 1>
+using column_vector = matrix<Type, Row, decltype(Row){1}>;
+//! @}
+
+//! @name Deduction Guides
+//! @{
+template <typename Type> matrix(Type) -> matrix<Type, 1, 1>;
+
+template <typename Type, auto Row, auto Column>
+matrix(const Type (&)[Row][Column]) -> matrix<Type, Row, Column>;
+
+template <typename Type, auto Row>
+matrix(const Type (&)[Row]) -> matrix<Type, Row, 1>; // column_vector
+//! @}
+
+//! @name Algebraic Named Values
+//! @{
+//! @brief Identity
+template <typename Type>
+inline constexpr Type identity_v<matrix<Type, 1, 1>>{1.0};
+
+template <typename Type, auto Row, auto Column>
+inline constexpr matrix<Type, Row, Column>
+    identity_v<matrix<Type, Row, Column>>{[] {
+      matrix<Type, Row, Column> result;
+
+      // Combine!
+      if constexpr (Row < Column) {
+        for (decltype(Row) k{0}; k < Row; ++k) {
+          result.data[k][k] = 1.0;
+        }
+      } else {
+        for (decltype(Column) k{0}; k < Column; ++k) {
+          result.data[k][k] = 1.0;
+        }
+      }
+
+      return result;
+    }()};
+
+//! @brief Zero
+template <typename Type> inline constexpr Type zero_v<matrix<Type, 1, 1>>{};
+
+template <typename Type, auto Row, auto Column>
+inline constexpr matrix<Type, Row, Column> zero_v<matrix<Type, Row, Column>>{};
+//! @}
+
+template <typename Type, auto Row, auto Column, auto Size>
+[[nodiscard]] inline constexpr auto
+operator*(const matrix<Type, Row, Size> &lhs,
+          const matrix<Type, Size, Column> &rhs) {
+  matrix<Type, Row, Column> result;
+
+  for (decltype(Row) i{0}; i < Row; ++i) {
+    for (decltype(Column) j{0}; j < Column; ++j) {
+      for (decltype(Size) k{0}; k < Size; ++k) {
+        result.data[i][j] += lhs.data[i][k] * rhs.data[k][j];
+      }
+    }
+  }
+
+  return result;
+}
+
+template <typename Type>
+[[nodiscard]] inline constexpr auto operator*(arithmetic auto lhs,
+                                              const matrix<Type, 1, 1> rhs) {
+  return lhs * rhs.data[0][0];
+}
+
+template <typename Type, auto Column>
+[[nodiscard]] inline constexpr auto operator*(arithmetic auto lhs,
+                                              matrix<Type, 1, Column> rhs) {
+  matrix<Type, 1, Column> result;
+
+  for (decltype(Column) j{0}; j < Column; ++j) {
+    result.data[0][j] = lhs * rhs.data[0][j];
+  }
+
+  return result;
+}
+
+template <typename Type, auto Row, auto Column>
+inline constexpr auto &operator*=(matrix<Type, Row, Column> &lhs,
+                                  arithmetic auto rhs) {
+  for (decltype(Row) i{0}; i < Row; ++i) {
+    for (decltype(Column) j{0}; j < Column; ++j) {
+      lhs.data[i][j] *= rhs;
+    }
+  }
+
+  return lhs;
+}
+
+template <typename Type, auto Row, auto Column>
+[[nodiscard]] inline constexpr auto operator*(matrix<Type, Row, Column> lhs,
+                                              arithmetic auto rhs) {
+  return lhs *= rhs;
+}
+
+template <typename Type, auto Row, auto Column>
+[[nodiscard]] inline constexpr auto
+operator+(const matrix<Type, Row, Column> &lhs,
+          const matrix<Type, Row, Column> &rhs) {
+  matrix<Type, Row, Column> result{lhs};
+
+  for (decltype(Row) i{0}; i < Row; ++i) {
+    for (decltype(Column) j{0}; j < Column; ++j) {
+      result.data[i][j] += rhs.data[i][j];
+    }
+  }
+
+  return result;
+}
+
+template <typename Type>
+[[nodiscard]] inline constexpr auto operator+(arithmetic auto lhs,
+                                              matrix<Type, 1, 1> rhs) {
+  return lhs + rhs.data[0][0];
+}
+
+template <typename Type>
+[[nodiscard]] inline constexpr auto operator+(matrix<Type, 1, 1> lhs,
+                                              arithmetic auto rhs) {
+  return lhs.data[0][0] + rhs;
+}
+
+template <typename Type, auto Row, auto Column>
+[[nodiscard]] inline constexpr auto
+operator-(const matrix<Type, Row, Column> &lhs,
+          const matrix<Type, Row, Column> &rhs) {
+  matrix<Type, Row, Column> result{lhs};
+
+  for (decltype(Row) i{0}; i < Row; ++i) {
+    for (decltype(Column) j{0}; j < Column; ++j) {
+      result.data[i][j] -= rhs.data[i][j];
+    }
+  }
+
+  return result;
+}
+
+template <typename Type>
+[[nodiscard]] inline constexpr auto operator-(arithmetic auto lhs,
+                                              const matrix<Type, 1, 1> &rhs) {
+  return lhs - rhs.data[0][0];
+}
+
+template <typename Type, auto Row>
+[[nodiscard]] inline constexpr auto operator/(const matrix<Type, Row, 1> &lhs,
+                                              arithmetic auto rhs) {
+  matrix<Type, Row, 1> result{lhs};
+
+  for (decltype(Row) i{0}; i < Row; ++i) {
+    result.data[i][0] /= rhs;
+  }
+
+  return result;
+}
+
+template <typename Type, auto Row, auto Column>
+[[nodiscard]] inline constexpr auto
+transpose(const matrix<Type, Row, Column> &lhs) {
+  matrix<Type, Column, Row> result;
+
+  for (decltype(Row) i{0}; i < Row; ++i) {
+    for (decltype(Column) j{0}; j < Column; ++j) {
+      result.data[j][i] = lhs.data[i][j];
+    }
+  }
+
+  return result;
+}
+} // namespace fcarouge
+
+#endif // FCAROUGE_LINALG_HPP
