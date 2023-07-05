5ac0e82f881f1263401ac95e301eb5e44a8c9b8f
FrancoisCarouge <francois.carouge@gmail.com>
2023-07-04 20:46:26 -0700

    [utility] extend type deducer for more algebra backends

--- include/fcarouge/internal/utility.hpp
+++ include/fcarouge/internal/utility.hpp
@@ -110,6 +110,9 @@ template <typename Type> struct not_implemented {
   static_assert(none, "This type is not implemented. See message.");
 };
 
+inline constexpr auto adl_transpose{
+    [](const auto &value) { return transpose(value); }};
+
 struct transpose final {
   template <arithmetic Arithmetic>
   [[nodiscard]] inline constexpr auto
@@ -117,10 +120,17 @@ struct transpose final {
     return value;
   }
 
+  //! @todo Remove and always rely on ADL and require implementation in linear
+  //! algebra support?
   template <typename Matrix>
     requires requires(Matrix value) { value.transpose(); }
   [[nodiscard]] inline constexpr auto operator()(const Matrix &value) const {
     return value.transpose();
+  }
+
+  template <typename Matrix>
+  [[nodiscard]] inline constexpr auto operator()(const Matrix &value) const {
+    return adl_transpose(value);
   }
 };
 
@@ -132,13 +142,39 @@ struct transpose final {
 //! `rhs` row size are the resulting type's column and row sizes, respectively:
 //! Lhs [m by n] and Rhs [o by n] -> Result [m by o].
 //! @todo Is there a better, simpler, canonical, standard way of doing this type
-//! deduction?
+//! deduction? For example, by doing it directly from the operation itself?
+//! There could be simplicity and performance benefits?
 struct deducer final {
   // Built-in, arithmetic, standard division support.
   template <arithmetic Lhs, arithmetic Rhs>
   [[nodiscard]] inline constexpr auto operator()(const Lhs &lhs,
                                                  const Rhs &rhs) const
       -> decltype(lhs / rhs);
+
+  // Type-erased matrix first party linear algebra support.
+  template <template <typename, auto, auto> typename Matrix, typename Type,
+            auto M, auto N, auto O>
+    requires(M > 1 || O > 1)
+  [[nodiscard]] inline constexpr auto
+  operator()(const Matrix<Type, M, N> &lhs, const Matrix<Type, O, N> &rhs) const
+      -> Matrix<Type, M, O>;
+
+  template <template <typename, auto, auto> typename Matrix, typename Type,
+            auto N>
+  [[nodiscard]] inline constexpr auto
+  operator()(const Matrix<Type, 1, N> &lhs, const Matrix<Type, 1, N> &rhs) const
+      -> Type;
+
+  template <template <typename, auto, auto> typename Lhs, typename Type, auto M>
+  [[nodiscard]] inline constexpr auto operator()(const Lhs<Type, M, 1> &lhs,
+                                                 arithmetic auto rhs) const
+      -> Lhs<Type, M, 1>;
+
+  //! @todo Coerce type and arithmetic to be the same?
+  template <template <typename, auto, auto> typename Rhs, typename Type, auto O>
+  [[nodiscard]] inline constexpr auto
+  operator()(arithmetic auto lhs, const Rhs<Type, O, 1> &rhs) const
+      -> Rhs<Type, 1, O>;
 
   // Type-erased Eigen third party linear algebra support.
   template <eigen Lhs, eigen Rhs>
