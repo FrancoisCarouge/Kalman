08c344f4ca54f10204262e09c3e85401d6595962
FrancoisCarouge <francois.carouge@gmail.com>
2024-10-12 21:22:21 -0700

    [algorithm] remove unused

--- include/fcarouge/kalman.hpp
+++ include/fcarouge/kalman.hpp
@@ -46,7 +46,6 @@ For more information, please refer to <https://unlicense.org> */
 //! utilities, and documentation. Only this header file is intended for
 //! inclusion in third party software.
 
-#include "algorithm.hpp"
 #include "internal/factory.hpp"
 #include "internal/format.hpp"
 #include "utility.hpp"
