750af39649419e05459148d32def494ac04ff70b
FrancoisCarouge <francois.carouge@gmail.com>
2025-01-18 20:03:47 -0800

    [test] strongly typed test for expression template correctness

--- test/kalman_constructor_default_5x1x1.cpp
+++ test/kalman_constructor_default_5x1x1.cpp
@@ -49,14 +49,13 @@ template <auto Row, auto Column> using matrix = matrix<double, Row, Column>;
 //! @test Verifies default values are initialized for multi-dimension filters,
 //! single output and input edge case.
 [[maybe_unused]] auto test{[] {
+  const matrix<1, 5> i1x5{identity<matrix<1, 5>>};
+  const matrix<5, 1> i5x1{identity<matrix<5, 1>>};
+  const matrix<5, 5> i5x5{identity<matrix<5, 5>>};
+  const vector<5> z5x1{zero<vector<5>>};
+  const matrix<5, 5> z5x5{zero<matrix<5, 5>>};
   kalman filter{state{vector<5>{0.0, 0.0, 0.0, 0.0, 0.0}}, output<double>,
                 input<double>};
-
-  const auto i1x5{identity<matrix<1, 5>>};
-  const auto i5x1{identity<matrix<5, 1>>};
-  const auto i5x5{identity<matrix<5, 5>>};
-  const auto z5x1{zero<vector<5>>};
-  const auto z5x5{zero<matrix<5, 5>>};
 
   assert(filter.f() == i5x5);
   assert(filter.g() == i5x1);
--- test/kalman_constructor_default_5x1x3.cpp
+++ test/kalman_constructor_default_5x1x3.cpp
@@ -49,16 +49,15 @@ template <auto Row, auto Column> using matrix = matrix<double, Row, Column>;
 //! @test Verifies default values are initialized for multi-dimension filters,
 //! single output edge case.
 [[maybe_unused]] auto test{[] {
+  const vector<3> z3x1{zero<vector<3>>};
+  const matrix<1, 5> i1x5{identity<matrix<1, 5>>};
+  const matrix<5, 3> i5x3{identity<matrix<5, 3>>};
+  const matrix<5, 1> i5x1{identity<matrix<5, 1>>};
+  const matrix<5, 5> i5x5{identity<matrix<5, 5>>};
+  const vector<5> z5x1{zero<vector<5>>};
+  const matrix<5, 5> z5x5{zero<matrix<5, 5>>};
   kalman filter{state{vector<5>{0.0, 0.0, 0.0, 0.0, 0.0}}, output<double>,
                 input<vector<3>>};
-
-  const auto z3x1{zero<vector<3>>};
-  const auto i1x5{identity<matrix<1, 5>>};
-  const auto i5x3{identity<matrix<5, 3>>};
-  const auto i5x1{identity<matrix<5, 1>>};
-  const auto i5x5{identity<matrix<5, 5>>};
-  const auto z5x1{zero<vector<5>>};
-  const auto z5x5{zero<matrix<5, 5>>};
 
   assert(filter.f() == i5x5);
   assert(filter.g() == i5x3);
