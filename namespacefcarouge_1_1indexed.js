3d156a103ac4ff6835b1ee841e0dd382ef989fec
FrancoisCarouge <FrancoisCarouge@users.noreply.github.com>
2025-02-03 01:26:40 +0000

    deploy: 34e79259321ecd91d39b6a0c07985db1b63c74a2

--- namespacefcarouge_1_1indexed.js
+++ namespacefcarouge_1_1indexed.js
@@ -1,9 +1,9 @@
 var namespacefcarouge_1_1indexed =
 [
     [ "matrix", "structfcarouge_1_1indexed_1_1matrix.xhtml", "structfcarouge_1_1indexed_1_1matrix" ],
+    [ "column_vector", "namespacefcarouge_1_1indexed.xhtml#a011e38c672db274168c0803a84ff586f", null ],
     [ "element_t", "namespacefcarouge_1_1indexed.xhtml#abe6bce60ee39e4e437143c1684cf4f4e", null ],
-    [ "indexed_column_vector", "namespacefcarouge_1_1indexed.xhtml#a6a70b0b4558bc573e8816f4c59ea91f0", null ],
-    [ "indexed_row_vector", "namespacefcarouge_1_1indexed.xhtml#af3a6049a52c6cb96d8bcbd57b9f84125", null ],
+    [ "row_vector", "namespacefcarouge_1_1indexed.xhtml#a8d7247e277a36773484a43caa8cc3f9f", null ],
     [ "operator*", "namespacefcarouge_1_1indexed.xhtml#ac9968aef067da8f2a348ea0f3124f927", null ],
     [ "operator*", "namespacefcarouge_1_1indexed.xhtml#ac0551e9efa1af2b91988881bebf4eaca", null ],
     [ "operator*", "namespacefcarouge_1_1indexed.xhtml#aa8199abf2d777c2e4657fe578f730b81", null ],
--- search/all_14.js
+++ search/all_14.js
@@ -12,7 +12,7 @@ var searchData=
   ['responsibilities_9',['Our Responsibilities',['../md__2github_2workspace_2_c_o_d_e___o_f___c_o_n_d_u_c_t.xhtml#our-responsibilities',1,'']]],
   ['results_10',['Results',['../md__2github_2workspace_2benchmark_2_r_e_a_d_m_e.xhtml#results',1,'']]],
   ['row_11',['Row',['../structfcarouge_1_1matrix.xhtml#a0a995e0b4a122d37f327be9b065d8136',1,'fcarouge::matrix']]],
-  ['row_5fvector_12',['row_vector',['../namespacefcarouge_1_1eigen.xhtml#adec4afd23d0a51e3f79d35c0bdd5f39b',1,'fcarouge::eigen::row_vector'],['../namespacefcarouge.xhtml#a7426c99fa5df210b90d63cd32c0b04f4',1,'fcarouge::row_vector'],['../namespacefcarouge_1_1naive.xhtml#a653b2773460ca94d5e356f3eefade70d',1,'fcarouge::naive::row_vector']]],
+  ['row_5fvector_12',['row_vector',['../namespacefcarouge_1_1eigen.xhtml#adec4afd23d0a51e3f79d35c0bdd5f39b',1,'fcarouge::eigen::row_vector'],['../namespacefcarouge_1_1indexed.xhtml#a8d7247e277a36773484a43caa8cc3f9f',1,'fcarouge::indexed::row_vector'],['../namespacefcarouge.xhtml#a7426c99fa5df210b90d63cd32c0b04f4',1,'fcarouge::row_vector'],['../namespacefcarouge_1_1naive.xhtml#a653b2773460ca94d5e356f3eefade70d',1,'fcarouge::naive::row_vector']]],
   ['rowindexes_13',['RowIndexes',['../structfcarouge_1_1indexed_1_1matrix.xhtml#a30d5f40ae1b6ce6d145494b0a8f459c3',1,'fcarouge::indexed::matrix']]],
   ['run_14',['Development Build &amp; Run',['../md__2github_2workspace_2_i_n_s_t_a_l_l.xhtml#development-build--run',1,'']]]
 ];
