28fbe576e547b287bbb185ca7bbf4055fbfeceff
FrancoisCarouge <francois.carouge@gmail.com>
2025-03-05 21:37:11 -0800

    [sample] organize unit samples

---
+++ sample/kf_1x1x0_building_height_unit.cpp
@@ -0,0 +1,118 @@
+/*  __          _      __  __          _   _
+| |/ /    /\   | |    |  \/  |   /\   | \ | |
+| ' /    /  \  | |    | \  / |  /  \  |  \| |
+|  <    / /\ \ | |    | |\/| | / /\ \ | . ` |
+| . \  / ____ \| |____| |  | |/ ____ \| |\  |
+|_|\_\/_/    \_\______|_|  |_/_/    \_\_| \_|
+
+Kalman Filter
+Version 0.4.0
+https://github.com/FrancoisCarouge/Kalman
+
+SPDX-License-Identifier: Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org> */
+
+#include "fcarouge/kalman.hpp"
+#include "fcarouge/unit.hpp"
+
+#include <cassert>
+
+namespace fcarouge::sample {
+namespace {
+//! @brief Estimating the height of a building.
+//!
+//! @copyright This example is transcribed from KalmanFilter.NET copyright Alex
+//! Becker.
+//! @copyright This example also transcribes Mateusz Pusz's mp-units Kalman
+//! filter sample.
+//!
+//! @see https://www.kalmanfilter.net/kalman1d.html#ex5
+//! @see
+//! https://github.com/mpusz/mp-units/blob/master/example/kalman_filter/kalman_filter-example_5.cpp
+//!
+//! @details Assume that we would like to estimate the height of a building
+//! using a very imprecise altimeter. We know for sure, that the building height
+//! doesn’t change over time, at least during the short measurement process. The
+//! true building height is 50 meters. The altimeter measurement error (standard
+//! deviation) is 5 meters. The set of ten measurements is: 48.54m, 47.11m,
+//! 55.01m, 55.15m, 49.89m, 40.85m, 46.72m, 50.05m, 51.27m, 49.95m.
+//!
+//! @image html ./sample/image/kf_1x1x0_building_height.svg
+//!
+//! @example kf_1x1x0_building_height_unit.cpp
+[[maybe_unused]] auto sample{[] {
+  // A one-dimensional filter, constant system dynamic model.
+  kalman filter{// One can estimate the building height simply by looking at it.
+                // The estimated state building height is: X = 60 meters.
+                state{height{60. * m}},
+                // The building height measurement Z in meters.
+                output<height>,
+                // A human’s estimation error (standard deviation) is about 15
+                // meters: σ = 15. Consequently the variance is σ^2 = 225. The
+                // estimate uncertainty is: P = 225 m^2.
+                estimate_uncertainty{225. * m2},
+                // Since the standard deviation σ of the altimeter measurement
+                // error is 5, the variance σ^2 would be 25, thus the
+                // measurement, output uncertainty is: R = 25 m^2.
+                output_uncertainty{25. * m2}};
+
+  assert(height{60. * m} == filter.x() &&
+         "Since our system's dynamic model is constant, i.e. the building "
+         "doesn't change its height: 60 meters.");
+  assert(225. * m2 == filter.p() &&
+         "The extrapolated estimate uncertainty (variance) also doesn't "
+         "change: 225");
+
+  // Now, we shall predict the next state based on the initialization values.
+  // Note: The prediction operation needs not be performed since the process
+  // noise covariance Q is null in this example.
+  // Measure and update: the first measurement is: z1 = 48.54m.
+  filter.update(height{48.54 * m});
+
+  // And so on.
+  filter.update(height{47.11 * m});
+  filter.update(height{55.01 * m});
+  filter.update(height{55.15 * m});
+  filter.update(height{49.89 * m});
+  filter.update(height{40.85 * m});
+  filter.update(height{46.72 * m});
+  filter.update(height{50.05 * m});
+  filter.update(height{51.27 * m});
+  filter.update(height{49.95 * m});
+
+  // After 10 measurements the filter estimates the height of the building
+  // at 49.57m.
+  assert(abs(1 - filter.x() / height{49.57 * m}) < 0.001 &&
+         "After 10 measurement and update iterations, the building estimated "
+         "height is: 49.57m.");
+
+  //! @todo Add format printing, verification.
+
+  return 0;
+}()};
+} // namespace
+} // namespace fcarouge::sample
--- sample/kf_6x2x0_vehicle_location_unit.cpp
+++ sample/kf_6x2x0_vehicle_location_unit.cpp
@@ -63,16 +63,41 @@ template <auto... Reference>
 inline fcarouge::output_t<vector<Reference...>> output{
     fcarouge::output<vector<Reference...>>};
 
-//! @test Verifies compatibility with the `mp-units` quantities and units
-//! library for C++ in the case of an algebraic 6x2x0 filter estimating the
-//! vehicle location.
+//! @brief Estimating the vehicle location.
 //!
-//! @details See the sample for details.
-[[maybe_unused]] auto test{[] {
+//! @copyright This example is transcribed from KalmanFilter.NET copyright Alex
+//! Becker.
+//!
+//! @see https://www.kalmanfilter.net/multiExamples.html#ex9
+//!
+//! @details In this example, we would like to estimate the location of the
+//! vehicle in the XY plane. The vehicle has an onboard location sensor that
+//! reports X and Y coordinates of the system. We assume constant acceleration
+//! dynamics. In this example we don't have a control variable u since we don't
+//! have control input. Let us assume a vehicle moving in a straight line in the
+//! X direction with a constant velocity. After traveling 400 meters the vehicle
+//! turns right, with a turning radius of 300 meters. During the turning
+//! maneuver, the vehicle experiences acceleration due to the circular motion
+//! (an angular acceleration). The measurements period: Δt = 1s (constant).
+//!
+//! @example kf_6x2x0_vehicle_location_unit.cpp
+[[maybe_unused]] auto sample{[] {
+  // A 6x2x0 filter, constant acceleration dynamic model, no control.
   kalman filter{
+      // The state X is chosen to be the position, velocity, acceleration in the
+      // XY plane: [px, vx, ax, py, vy, ay]. We don't know the vehicle location;
+      // we will set initial position, velocity and acceleration to 0.
       state{0. * m, 0. * m / s, 0. * m / s2, 0. * m, 0. * m / s, 0. * m / s2},
+      // The vehicle has an onboard location sensor that reports output Z as X
+      // and Y coordinates of the system.
       output<m, m>,
+      // The estimate uncertainty matrix P.
+      // Since our initial state vector is a guess, we will set a very high
+      // estimate uncertainty. The high estimate uncertainty results in a high
+      // Kalman Gain, giving a high weight to the measurement.
       estimate_uncertainty{500. * identity<estimate_uncertainty::type>},
+      // The process uncertainty noise matrix Q, constant, computed in place,
+      // with  random acceleration standard deviation: σa = 0.2 m.s^-2.
       process_uncertainty{[]() {
         process_uncertainty::type value{identity<process_uncertainty::type>};
         value.at<0, 0>() = 0.25 * m2;
@@ -91,13 +116,27 @@ inline fcarouge::output_t<vector<Reference...>> output{
         value.at<5, 4>() = 1. * m2 / s3;
         return 0.2 * 0.2 * value;
       }()},
+      // The output uncertainty matrix R. Assume that the x and y measurements
+      // are uncorrelated, i.e. error in the x coordinate measurement doesn't
+      // depend on the error in the y coordinate measurement. In real-life
+      // applications, the measurement uncertainty can differ between
+      // measurements. In many systems the measurement uncertainty depends on
+      // the measurement SNR (signal-to-noise ratio), angle between sensor (or
+      // sensors) and target, signal frequency and many other parameters.
+      // For the sake of the example simplicity, we will assume a constant
+      // measurement uncertainty: R1 = R2...Rn-1 = Rn = R The measurement error
+      // standard deviation: σxm = σym = 3m. The variance 9.
       output_uncertainty{{9. * m2, 0. * m2}, {0. * m2, 9. * m2}},
+      // The output model matrix H. The dimension of zn is 2x1 and the dimension
+      // of xn is 6x1. Therefore the dimension of the observation matrix H shall
+      // be 2x6.
       output_model{[]() {
         output_model::type value{zero<output_model::type>};
         value.at<0, 0>() = 1. * m2;
         value.at<1, 3>() = 1. * m2;
         return value;
       }()},
+      // The state transition matrix F would be:
       state_transition{[]() {
         state_transition::type value{identity<state_transition::type>};
         value.at<0, 1>() = 1. * m2 / s;
@@ -109,12 +148,16 @@ inline fcarouge::output_t<vector<Reference...>> output{
         return value;
       }()}};
 
+  // Now we can predict the next state based on the initialization values.
   filter.predict();
+
+  // The measurement values: z1 = [-393.66 m, 300.4 m]
   filter.update(-393.66 * m, 300.4 * m);
   filter.predict();
   filter.update(-375.93 * m, 301.78 * m);
   filter.predict();
 
+  // Verify the example estimated state at 0.1% accuracy.
   assert(abs(1 - filter.x().at<0>() / (-277.8 * m)) < 0.001 &&
          abs(1 - filter.x().at<1>() / (148.3 * m / s)) < 0.001 &&
          abs(1 - filter.x().at<2>() / (94.5 * m / s2)) < 0.001 &&
@@ -124,6 +167,7 @@ inline fcarouge::output_t<vector<Reference...>> output{
          "The state estimates expected at 0.1% accuracy.");
 
   //! @todo Add format verification.
+  //! @todo Complete the example from the original data.
 
   return 0;
 }()};
