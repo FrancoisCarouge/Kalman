3fe8fb21cfe13d1243fa8339b290994ca070d40a
FrancoisCarouge <francois.carouge@gmail.com>
2025-05-23 13:34:25 -0700

    [linalg] typed linear algebra regression

--- include/fcarouge/kalman_internal/x_z_p_r_f.hpp
+++ include/fcarouge/kalman_internal/x_z_p_r_f.hpp
@@ -47,10 +47,10 @@ template <typename State> struct x_z_p_r_f {
   using output = State;
   using estimate_uncertainty = ᴀʙᵀ<state, state>;
   using output_uncertainty = ᴀʙᵀ<output, output>;
-  using state_transition = ᴀʙᵀ<state, state>;
+  using state_transition = evaluate<quotient<state, state>>;
   using innovation = evaluate<difference<output, state>>;
   using innovation_uncertainty = output_uncertainty;
-  using gain = evaluate<quotient<estimate_uncertainty, innovation_uncertainty>>;
+  using gain = evaluate<quotient<state, innovation>>;
 
   static inline const auto i{one<gain>};
 
