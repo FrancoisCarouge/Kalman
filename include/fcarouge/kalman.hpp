c59d4b874d02319e406c99a50c3adce10d117e4e
FrancoisCarouge <francois.carouge@gmail.com>
2025-11-27 14:03:36 -0800

    [filter] simplify and generalize characteristics accesses

--- include/fcarouge/kalman.hpp
+++ include/fcarouge/kalman.hpp
@@ -245,24 +245,14 @@ public:
   //! @name Public Characteristics Member Functions
   //! @{
 
-  //! @brief Returns the state estimate column vector X.
+  //! @brief Read, write the estimated state column vector X.
   //!
-  //! @return The state estimate column vector X.
-  //!
-  //! @complexity Constant.
-  //!
-  //! @note Overloading the operator dot would have been nice had it existed.
-  inline constexpr auto &&x(this auto &&self);
-
-  //! @brief Sets the state estimate column vector X.
-  //!
-  //! @param value The first copied initializer used to set the state estimate
-  //! column vector X.
-  //! @param values The optional second and other copied initializers to set the
-  //! state estimate column vector X.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the estimated state
+  //! column vector X characteristic.
   //!
   //! @complexity Constant.
-  inline constexpr void x(const auto &value, const auto &...values);
+  inline constexpr decltype(auto) x(this auto &&self, const auto &...values);
 
   //! @brief Returns the last observation column vector Z.
   //!
@@ -281,194 +271,109 @@ public:
   inline constexpr const auto &u() const
     requires(kalman_internal::has_input<Filter>);
 
-  //! @brief Returns the estimated covariance matrix P.
+  //! @brief Read, write the estimated covariance matrix P.
   //!
-  //! @return The estimated correlated variance matrix P.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the estimated
+  //! covariance matrix P characteristic.
   //!
   //! @complexity Constant.
-  inline constexpr auto &&p(this auto &&self);
+  inline constexpr decltype(auto) p(this auto &&self, const auto &...values);
 
-  //! @brief Sets the estimated covariance matrix P.
+  //! @brief Read, write the process noise covariance matrix or function Q.
   //!
-  //! @param value The first copied initializer used to set the estimated
-  //! covariance matrix P.
-  //! @param values The optional second and other copied initializers to set the
-  //! estimated covariance matrix P.
-  //!
-  //! @complexity Constant.
-  inline constexpr void p(const auto &value, const auto &...values);
-
-  //! @brief Returns the process noise covariance matrix Q.
-  //!
-  //! @return The process noise correlated variance matrix Q.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the process noise
+  //! covariance matrix Q characteristic. The characteristic may also be a
+  //! callable of the form `process_uncertainty(const state &, const
+  //! PredictionTypes &...)`.
   //!
   //! @complexity Constant.
-  inline constexpr auto &&q(this auto &&self)
+  inline constexpr decltype(auto) q(this auto &&self, const auto &...values)
     requires(kalman_internal::has_process_uncertainty<Filter>);
 
-  //! @brief Sets the process noise covariance matrix Q.
+  //! @brief Read, write the observation noise covariance matrix R.
   //!
-  //! @param value The first copied initializer used to set the process noise
-  //! covariance matrix Q is of type `process_uncertainty` and the function is
-  //! of the form `process_uncertainty(const state &, const PredictionTypes
-  //! &...)`. The copied process noise covariance matrix Q or the copied target
-  //! Callable object (function object, pointer to function, reference to
-  //! function, pointer to member function, or pointer to data member) that will
-  //! be bound to the prediction arguments and called by the filter to compute
-  //! the process noise covariance matrix Q on prediction steps.
-  //! @param values The optional second and other copied initializers to set the
-  //! process noise covariance matrix Q.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the observation
+  //! noise covariance matrix R characteristic. The characteristic may also be a
+  //! callable of the form `output_uncertainty(const state &, const output &,
+  //! const UpdateTypes &...)`.
   //!
   //! @complexity Constant.
-  inline constexpr void q(const auto &value, const auto &...values)
-    requires(kalman_internal::has_process_uncertainty<Filter>);
-
-  //! @brief Returns the observation noise covariance
-  //! matrix R.
-  //!
-  //! @details The variance there is in each measurement.
-  //!
-  //! @return The observation noise correlated variance matrix R.
-  //!
-  //! @complexity Constant.
-  inline constexpr auto &&r(this auto &&self)
+  inline constexpr decltype(auto) r(this auto &&self, const auto &...values)
     requires(kalman_internal::has_output_uncertainty<Filter>);
 
-  //! @brief Sets the observation noise covariance matrix R.
+  //! @brief Read, write the state transition matrix F.
   //!
-  //! @param value The first copied initializer used to set the observation
-  //! noise covariance matrix R is of type `output_uncertainty` and the function
-  //! is of the form `output_uncertainty(const state &, const output &, const
-  //! UpdateTypes &...)`. The copied observation noise covariance matrix R or
-  //! the copied target Callable object (function object, pointer to function,
-  //! reference to function, pointer to member function, or pointer to data
-  //! member) that will be called by the filter to compute the observation noise
-  //! covariance matrix R on prediction steps.
-  //! @param values The optional second and other copied initializers to set the
-  //! observation noise covariance matrix R.
-  //!
-  //! @complexity Constant.
-  inline constexpr void r(const auto &value, const auto &...values)
-    requires(kalman_internal::has_output_uncertainty<Filter>);
-
-  //! @brief Returns the state transition matrix F.
-  //!
-  //! @return The state transition matrix F.
-  //!
-  //! @complexity Constant.
-  inline constexpr auto &&f(this auto &&self)
-    requires(kalman_internal::has_state_transition<Filter>);
-
-  //! @brief Sets the state transition matrix F.
-  //!
-  //! @details The state transition matrix F is of type `state_transition` and
-  //! the function is of the form `state_transition(const state &, const input
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the state transition
+  //! matrix F characteristic. The characteristic may also be a callable of the
+  //! form `state_transition(const state &, const input
   //! &, const PredictionTypes &...)`. For non-linear system, or extended
   //! filter, F is the Jacobian of the state transition function: `F = ∂f/∂X =
   //! ∂fj/∂xi` that is each row i contains the derivatives of the state
   //! transition function for every element j in the state column vector X.
   //!
-  //! @param value The first copied initializer used to set the copied state
-  //! transition matrix F or the copied target Callable object (function object,
-  //! pointer to function, reference to function, pointer to member function, or
-  //! pointer to data member) that will be bound to the prediction arguments and
-  //! called by the filter to compute the state transition matrix F function on
-  //! prediction steps.
-  //! @param values The optional second and other copied initializers to set the
-  //! state transition matrix F.
-  //!
   //! @complexity Constant.
-  inline constexpr void f(const auto &value, const auto &...values)
+  inline constexpr decltype(auto) f(this auto &&self, const auto &...values)
     requires(kalman_internal::has_state_transition<Filter>);
 
-  //! @brief Returns the observation transition matrix H.
+  //! @brief Read, write the observation transition matrix H.
   //!
-  //! @details This member function is not present when the filter has no output
-  //! model.
-  //!
-  //! @return The observation, measurement transition matrix H.
-  //!
-  //! @complexity Constant.
-  inline constexpr auto &&h(this auto &&self)
-    requires(kalman_internal::has_output_model<Filter>);
-
-  //! @brief Sets the observation transition matrix H.
-  //!
-  //! @details The observation transition matrix H is of type `output_model` and
-  //! the function is of the form `output_model(const state &, const UpdateTypes
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the observation
+  //! transition matrix H characteristic. The characteristic may also be a
+  //! callable of the form `output_model(const state &, const UpdateTypes
   //! &...)`. For non-linear system, or extended filter, H is the Jacobian of
   //! the state observation function: `H = ∂h/∂X = ∂hj/∂xi` that is each row i
   //! contains the derivatives of the state observation function for every
   //! element j in the state column vector X. This member function is not
   //! present when the filter has no output model.
   //!
-  //! @param value The first copied initializer used to set the copied
-  //! observation transition matrix H or the copied target Callable object
-  //! (function object, pointer to function, reference to function, pointer to
-  //! member function, or pointer to data member) that will be bound to the
-  //! prediction arguments and called by the filter to compute the observation,
-  //! measurement transition matrix H on update steps.
-  //! @param values The optional second and other copied initializers to set the
-  //! observation transition matrix H.
-  //!
   //! @complexity Constant.
-  inline constexpr void h(const auto &value, const auto &...values)
+  inline constexpr decltype(auto) h(this auto &&self, const auto &...values)
     requires(kalman_internal::has_output_model<Filter>);
 
-  //! @brief Returns the control transition matrix G.
+  //! @brief Read, write the control transition matrix G.
   //!
-  //! @details This member function is not present when the filter has no input
-  //! control.
-  //!
-  //! @return The control transition matrix G.
-  //!
-  //! @todo Do we still need the non-const flavor?
-  //!
-  //! @complexity Constant.
-  inline constexpr auto &&g(this auto &&self)
-    requires(kalman_internal::has_input_control<Filter>);
-
-  //! @brief Sets the control transition matrix G.
-  //!
-  //! @details The control transition matrix G is of type `input_control` and
-  //! the function is of the form `input_control(const PredictionTypes &...)`.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the control
+  //! transition matrix G. characteristic. The characteristic may also be a
+  //! callable of the form `input_control(const PredictionTypes &...)`.
   //! This member function is not present when the filter has no input control.
   //!
-  //! @param value The first copied initializer used to set the copied control
-  //! transition matrix G or the copied target Callable object (function object,
-  //! pointer to function, reference to function, pointer to member function, or
-  //! pointer to data member) that will be bound to the prediction arguments and
-  //! called by the filter to compute the control transition matrix G on
-  //! prediction steps.
-  //! @param values The optional second and other copied initializers to set the
-  //! control transition matrix G.
-  //!
   //! @complexity Constant.
-  inline constexpr void g(const auto &value, const auto &...values)
+  inline constexpr decltype(auto) g(this auto &&self, const auto &...values)
     requires(kalman_internal::has_input_control<Filter>);
 
-  //! @brief Returns the gain matrix K.
+  //! @brief Read, write the gain matrix K.
   //!
-  //! @return The gain matrix K.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the gain matrix K
+  //! characteristic.
   //!
   //! @complexity Constant.
-  inline constexpr auto k() const -> const gain &;
+  inline constexpr decltype(auto) k(this auto &&self, const auto &...values);
 
-  //! @brief Returns the innovation column vector Y.
+  //! @brief Read, write the innovation column vector Y.
   //!
-  //! @return The innovation column vector Y.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the innovation
+  //! column vector Y characteristic.
   //!
   //! @complexity Constant.
-  //!
-  //! @todo Add measurement post-fit residual by default and example?
-  inline constexpr auto y() const -> const innovation &;
+  inline constexpr decltype(auto) y(this auto &&self, const auto &...values);
 
-  //! @brief Returns the innovation uncertainty matrix S.
+  //! @brief Read, write the innovation uncertainty matrix S.
   //!
-  //! @return The innovation uncertainty matrix S.
+  //! @param self Explicit object parameter. Internal implementation detail.
+  //! @param values The optional copied initializers to set the innovation
+  //! uncertainty matrix S characteristic.
   //!
   //! @complexity Constant.
-  inline constexpr auto s() const -> const innovation_uncertainty &;
+  inline constexpr decltype(auto) s(this auto &&self, const auto &...values);
+
   //! @}
 
   //! @name Public Filtering Member Functions
